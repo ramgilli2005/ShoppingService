f8ef35a2ba367d971d7ac11bc747b808cfab8e17
RAM <ramgilli2005@gmail.com>
2015-12-17 13:00:06 -0600

    Cache expiry

--- src/main/java/com/wpl/services/LoginService.java
+++ src/main/java/com/wpl/services/LoginService.java
@@ -43,7 +43,7 @@ public class LoginService {
 		} else{
 			log.info("Cache Miss");
 			ld = (LoginDetails) sf.getCurrentSession().get(LoginDetails.class, userDetails.getUserName());
-			mc.set(userDetails.getUserName(), 3600, ld);
+			mc.set(userDetails.getUserName(), 5, ld);
 		}
 			
 		userDetails.setStatus(ld.getStatus());
